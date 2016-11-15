6fe3ffe8027c23cb3b37bba77857c7f58d158dbd
Matt Le <ml9951@rit.edu>
2016-11-15 15:04:16 -0500

    Adding command line args to control filename, tablename, and creation

--- geojson2postgres.js
+++ geojson2postgres.js
@@ -125,13 +125,13 @@ exports.getSchema = getSchema
  * script to insert each feature into a database
  * @function 
  * @param {string} tablename - Name of table to create in the 
- * 		database (table should not already exist)
+ *    database (table should not already exist)
  * @param {string} file - Name of the input GeoJSON file
  * @param {Array<{name : string, type : string}>} schema - The schema
- * 		of the table to be created.  `name` is a column name and 
- * 		`type` is the type for that column
+ *    of the table to be created.  `name` is a column name and 
+ *    `type` is the type for that column
  * @param {function} emitRow - A function that takes in a single 
- * 		GeoJSON feature and returns an object where the keys are
+ *    GeoJSON feature and returns an object where the keys are
  *      column names defined in the schema and the values are 
  *      the values to be entered into each column
  */
@@ -148,14 +148,15 @@ function copyToDB (args) {
     return col.name
   }).join(',')
 
-  console.log('SET CLIENT_ENCODING TO UTF8;')
-  console.log('SET STANDARD_CONFORMING_STRINGS TO ON;')
   console.log('BEGIN;')
-  console.log(`DROP TABLE IF EXISTS ${args.tablename};`)
-  console.log(`CREATE TABLE "${args.tablename}" (${schema});`)
-  console.log(`SELECT AddGeometryColumn('','${args.tablename}','geom','4326','GEOMETRY',2);`)
+  if (args.create) {
+    console.log('SET CLIENT_ENCODING TO UTF8;')
+    console.log('SET STANDARD_CONFORMING_STRINGS TO ON;')
+    console.log(`DROP TABLE IF EXISTS ${args.tablename};`)
+    console.log(`CREATE TABLE "${args.tablename}" (${schema});`)
+    console.log(`SELECT AddGeometryColumn('','${args.tablename}','geom','4326','GEOMETRY',2);`)
+  }
   console.log(`COPY ${args.tablename} (${colNames}) FROM stdin;`)
-
   // Stream in the GeoJSON file
   var inStream = fs.createReadStream(args.file)
     .pipe(geojsonStream.parse()).pipe(es.mapSync(function (feature) {
@@ -167,7 +168,8 @@ function copyToDB (args) {
   }))
   inStream.on('end', function () {
     console.log('\\.')
-    console.log(`CREATE INDEX ON ${args.tablename} USING GIST(geom);`)
+    if (args.create)
+      console.log(`CREATE INDEX ON ${args.tablename} USING GIST(geom);`)
     console.log('COMMIT;\n')
   })
 }
